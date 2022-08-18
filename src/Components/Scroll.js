9bf2ea9110118ff6f3370978825966eb497105a2
Shreshth Sharma <shreshthsharma.contact@gmail.com>
2022-08-18 20:55:02 +0530

    changes

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export const Scroll=(prop)=>{
 return(
-    <div className='tc element' style={{overflowY: "scroll", height:"390px"}}>
+    <div className='tc element' style={{overflowY: "scroll", height:"500px"}}>
         {prop.children}
     </div>
 )
